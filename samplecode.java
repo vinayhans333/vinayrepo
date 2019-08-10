e48cdefb508c239536e12b57b51489471fb714d1
vinayhans333 <52851342+vinayhans333@users.noreply.github.com>
2019-08-10 14:30:29 +0530

    Update samplecode.java

--- samplecode.java
+++ samplecode.java
@@ -1,5 +1,5 @@
 class simple{
-    public static void main(string args[]) throws Interrupted Exception{
+    public static void main(string args[]) throws Interrupted Exception; {
     Thread.sleep(5000);
     System.out.printIn("Hello Java");
     }
